f240fb017943d929c14ccb7e62852a0392e13ee3
Attila Zsolt Somogyi <attila.zsolt.somogyi@gmail.com>
2023-04-18 19:14:07 +0200

    ci: add set backup repository

---
+++ set_backup_repository.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+function usage() {
+  echo "Usage: $0 [--username <username>]" 1>&2
+  exit 1
+}
+
+function git_remote_set_url_add_push_origin() {
+  git_url="$1"
+  git config --get-all remote.origin.pushurl | grep --quiet "$git_url" \
+    || git remote set-url --add --push origin "$git_url"
+}
+
+function main() {
+  options=$(getopt --name "$0" --options u: --longoptions username: -- "$@") || usage
+  eval set -- "$options"
+  while true; do
+    case "$1" in
+      --username)
+        shift
+        username="$1"
+        break
+        ;;
+      --)
+        shift
+        ;;
+      *)
+        usage
+        ;;
+    esac
+  done
+  repository_name=$(basename $(git rev-parse --show-toplevel))
+  github_repository_url="git@github.com:${username}/${repository_name}.git"
+  gitlab_repository_url="git@gitlab.com:${username}/${repository_name}.git"
+  bitbucket_repository_url="git@bitbucket.org:${username}/${repository_name}.git"
+  export -f git_remote_set_url_add_push_origin
+  printf "%s" "${github_repository_url} ${gitlab_repository_url} ${bitbucket_repository_url}" | parallel --delimiter ' ' --replace --jobs "$(nproc)" git_remote_set_url_add_push_origin "{}"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
